7e84371e7722918c98b16478d40dd5edfdbf42a4
= <=>
2023-07-05 20:31:38 +0600

    first login register route on api taken

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+Route::post('register',[RegisterController::class, 'register']);
+Route::post('login',[RegisterController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::resource('products', ProductController::class);
+}
+
+]);
